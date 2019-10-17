a5bfc534e0252dd4c5e4c408c7910e1c73d9acb1
Kunn Liu <abce1202@gmail.com>
2019-10-17 23:19:46 +0800

    v2.0.2  add filtering function

--- reviews.py
+++ reviews.py
@@ -4,9 +4,9 @@ count = 0
 with open('reviews.txt', 'r') as file:
 	for line in file:
 		data.append(line)
-		count += 1
-		if count % 1000 == 0:
-			print(len(data))
+#		count += 1
+#		if count % 1000 == 0:
+#			print(len(data))
 
 print('檔案讀取完了，總共有', len(data), '筆資料')
 
@@ -15,3 +15,10 @@ for line1 in data:
 	sum_len += len(line1)
 print('留言的平均長度為', sum_len / len(data))
 
+new = []
+for line2 in data:
+	if len(line2) < 100:
+		new.append(line2)
+print('共有', len(new), '筆留言長度小於100個字') #the算三個字
+print(new[0])
+print(new[1])
