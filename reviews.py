2b2c3b6496dd8603ed50d5e7c94fc85909e99fb7
Kunn Liu <abce1202@gmail.com>
2019-10-17 23:04:47 +0800

    firs version

--- reviews.py
+++ reviews.py
@@ -1,14 +1,33 @@
 #讀取reviews.txt檔
+#data = []
+#count = 0
+#with open('reviews.txt', 'r') as file:
+#	for line in file:
+#		data.append(line)
+#		count += 1
+#		if count % 1000 == 0:
+#			print(len(data))
+
+#print('檔案讀取完了，總共有', len(data), '筆資料')
+
+
 data = []
-count = 0
+#count = 0
 with open('reviews.txt', 'r') as file:
 	for line in file:
 		data.append(line)
-		count += 1
-		if count % 1000 == 0:
-			print(len(data))
-
-print(len(data))
-
-print(data[0])
+total = len(data)
+print(total)
+count = 0
+data1 = []
+summ = 0
+while count != total:
+	length = 0
+	length = len(data[count])
+	summ += length
+	aver = summ / total
+	count += 1
+	if count == total:
+		break
+print('平均長度為', aver)
 
